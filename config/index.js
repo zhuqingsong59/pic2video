f11526ea88e99bde5219bf1a221532a2dd849c8e
zqs <zhuqingsong@weihaizixun.com>
2022-02-03 21:16:32 +0800

    我要做出来的是个啥

--- config/index.js
+++ config/index.js
@@ -1,20 +1,28 @@
 const fs =  require('fs')
 const path = require('path')
 // 图片目录
-const picDir = path.join(__dirname, '../', './resources/album')
+const picDir = path.join(__dirname, '../', './resources/album/hmm')
 // 缓存目录
 const cacheDir = path.join(__dirname, '../', './cache/')
 // 视频输出目录
 const outputDir = path.join(__dirname, '../', './public/')
+// 封面
+const cover = path.join(__dirname, '../', './resources/cover/hmm.jpg')
+// 背景音乐
+const audio = path.join(__dirname, '../', './resources/audio/bg.mp3');
 // 配置信息
 const videoConfig = {
   // creator配置
   creatorConfig: {
+    cover,
     cacheDir,
     outputDir,
-    width: 640, // 视频宽度
-    height: 352, // 视频高度
+    width: 320, // 视频宽度
+    height: 600, // 视频高度
   },
+  audio,
+  albumHeight: 569,
+  albumWidth: 320,
   // 相册列表
   albumList: fs.readdirSync(picDir).map(item => path.join(picDir, item))
 }
--- index.js
+++ index.js
@@ -1,78 +1,40 @@
-// const config = require('./config')
-// console.log(config)
 
 const path = require('path');
 const colors = require('colors');
-const { FFScene, FFText, FFAlbum, FFImage, FFCreator } = require("ffcreator");
+const { FFScene, FFAudio, FFAlbum, FFCreator } = require("ffcreator");
 const outputDir = path.join(__dirname, './public/');
 const cacheDir = path.join(__dirname, './cache/');
-
-const img1 = path.join(__dirname, './resources/album/1.jpeg');
-const img2 = path.join(__dirname, './resources/album/2.jpeg');
-const img3 = path.join(__dirname, './resources/album/3.jpeg');
-const cover = path.join(__dirname, './resources/album/5.jpeg');
-
-const width = 640;
-const height = 500;
-
+const config = require('./config')
+console.log(config)
 // Create FFCreator instance
-const creator = new FFCreator({
-    cover,
-    cacheDir,
-    outputDir,
-    width,
-    height
-});
+const creator = new FFCreator(config.creatorConfig);
+
+creator.addAudio(new FFAudio({
+  path: config.audio,
+  volume: 0.9,
+  fadeIn: 4,
+  fadeOut: 4,
+  loop: true
+}));
 
 // Create scene2
-const scene1 = new FFScene();
-const scene2 = new FFScene();
-
-scene1.setBgColor("#3b3a98");
-scene1.setDuration(2);
-const img = new FFImage({ path: img2 });
-img.setXY(width/2, height/2);                // 设置位置
-img.setScale(2);                 // 设置缩放
-// img.setRotate(45);                  // 设置旋转
-img.setOpacity(0.3);                // 设置透明度
-img.setWH(width, height);              // 设置宽高
-img.addEffect('fadeInDown', 1, 0);
-scene1.addChild(img);
-creator.addChild(scene1);
-
-scene2.setBgColor("#3b3a98");
-scene2.setDuration(4);
-scene2.setTransition("GridFlip", 2);
-creator.addChild(scene2);
-
-// Create Text
-const text = new FFText({
-   text: "瓜子",
-   fontSize: 40,
-   x: width / 2,
-   y: 60
- });
-text.setColor('#ffffff');
-text.setBackgroundColor('#01003c');
-text.addEffect('fadeInUp', 1, 0);
-text.alignCenter();
-text.setStyle({ padding: 10 });
-scene2.addChild(text);
-
+const scene = new FFScene();
+scene.setDuration(config.albumList.length + 1)
+creator.addChild(scene);
 // Create a multi-photo Album
 const album = new FFAlbum({
-    list: [img1, img2, img3],   // Picture collection for album
-    x: width / 2,
-    y: height / 2,
-    width: width,
-    height: 300,
+    list: config.albumList,   // Picture collection for album
+    x: config.albumWidth / 2,
+    y: config.albumHeight / 2,
+    width: config.albumWidth,
+    height: config.albumHeight,
     showCover: true
 });
 album.setTransition('zoomIn');      // Set album switching animation
 album.setDuration(1);             // Set the stay time of a single sheet
-scene2.addChild(album);
+scene.addChild(album);
 
-creator.output(path.join(__dirname, "./public/cat.mp4"));
+creator.output(path.join(__dirname, "./public/hmm.mp4"));
 creator.start();        // Start processing
 creator.closeLog();     // Close log (including perf)
 
